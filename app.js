bc1ea548068c7db54f3408a82e02dbd39640adcb
Himank17Gupta <hemankgupta@gmail.com>
2020-02-06 09:56:40 +0530

    reached upto headers problem

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const fs=require('fs');
 const port = 4000;
 var bodyParser = require('body-parser');
 app.use(express.static('public'));
@@ -8,13 +9,23 @@ app.use(bodyParser.json());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization","Accept-Ranges:bytes");
+  res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
   next();
 });
 app.get('/', (req, res) => res.sendFile(__dirname+'/client.html'));
 
 app.use('/users',require('./controllers/upload'));
-app.get('/stream',(req,res)=>{console.log('stream called');res.sendFile(__dirname+'/manifest/SampleVideo_1280x720_1mb.mp4.mpd');});
+app.get('/stream',(req,res)=>{
+  console.log('stream called' );
+  var stat = fs.statSync(__dirname+'/manifest/SampleVideo_1280x720_5mb.mp4.mpd');
+  var total = stat.size;
+  console.log('ALL: ' + total);
+  res.header('Content-Length', total, 'Content-Type','video/mp4' );
+  //fs.createReadStream(__dirname+'/manifest/SampleVideo_1280x720_5mb.mp4.mpd').pipe(res);
+  
+    res.sendFile(__dirname+'/manifest/SampleVideo_1280x720_5mb.mp4.mpd');
+});
 
 app.use((req,res,next)=>{
     res.send('You typed Something Invalid...No Resource Found');
