8bdbcc42195d90dde0cbf52d6ade2bd26329fcb4
Himank Gupta <hemankgupta@gmail>
2020-02-06 12:57:23 +0530

    done fixes...not helpful enough

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header('Access-Control-Expose-Headers: X-From-Cache');
   next();
 });
 app.get('/', (req, res) => res.sendFile(__dirname+'/client.html'));
